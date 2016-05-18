b58c155f30e5946f3fe9396eea5d3d66ca81df52
clivealvares <clive.alvares@gmail.com>
2016-05-18 03:31:21 -0400

    Initial Commit

---
+++ site.js
@@ -0,0 +1,15 @@
+
+	jQuery(document).ready(function($) {
+		    
+		    // Localscrolling 
+    		$('#nav-main, .brand').localScroll();
+     		$('#news, .container').localScroll();
+   		    $('#banner').localScroll();
+    		
+            $('#banner').carousel({
+    			interval: 12000
+    		});
+
+	});
+	
+	
